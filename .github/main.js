d3899e1230b3d3a4e5e833c60d3713c83b71a7a4
Diana Man <madi22gc@student.ju.se>
2023-04-12 10:20:58 +0300

    update

--- .github/main.js
+++ .github/main.js
@@ -4,10 +4,15 @@ let inputField = document.getElementById('inputField');
 
 addATaskButton.addEventListener('click', function() {
     var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     tasksContainer.appendChild(paragraph);
     inputField.value = "";
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
+    })
+
+    paragraph.addEventListener('dblclick', function(){
+        tasksContainer.removeChild(paragraph);
     })
 })
