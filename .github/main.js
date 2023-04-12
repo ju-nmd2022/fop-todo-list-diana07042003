e744b3b3ca1c5d445f6b815f897362f714814fee
Diana Man <madi22gc@student.ju.se>
2023-04-12 12:07:25 +0300

    update

--- .github/main.js
+++ .github/main.js
@@ -1,32 +1,60 @@
 
 
 
-let addATaskButton = document.getElementById('addATask');
-let tasksContainer = document.getElementById('tasksContainer');
-let inputField = document.getElementById('inputField');
+const inputVal = document.getElementsByClassName('inputVal')[0];
 
-addATaskButton.addEventListener('click', function() {
-    var paragraph = document.createElement('p')
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    tasksContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
+ const addTaskBtn = document.getElementsByClassName('btn')[0];
+ 
 
-    paragraph.addEventListener('dblclick', function(){
-        tasksContainer.removeChild(paragraph);
-    })
-})
+addTaskBtn.addEventListener('click', function (){
+  
+if(inputVal.value.trim()!=0){
+       let localItems = JSON.parse( localStorage.getItem('localItem'))
+    if(localItems === null){
+         taskList = []
 
-
-
-const tasks = JSON.parse (localStorage.getItem('tasks'))
-
-if (tasks){
-    tasks.forEach((task) =>
-{
-    insertTaskIntoPage(task)
-})
+    }else{
+        taskList = localItems;
+    }
+    taskList.push(inputVal.value)
+    localStorage.setItem('localItem', JSON.stringify(taskList)); 
 }
+
+    showItem()
+})
+
+function showItem(){
+    let localItems = JSON.parse( localStorage.getItem('localItem'))
+    if(localItems === null){
+         taskList = []
+
+    }else{
+        taskList = localItems;
+    }
+
+//The next codes are inspired from the video https://youtu.be/q0-N_w0Op84
+let html = '';
+let itemShow = document.querySelector('.todoLists');
+taskList.forEach((data, index )=> {
+    
+
+    html += `
+    <div class="todoList">
+    <p class="pText">${data}</p>
+    <button class="deleteTask" onClick="deleteItem(${index})">x</button>
+    </div>
+    `
+})
+itemShow.innerHTML = html;
+}
+showItem()
+
+function deleteItem(index){
+    let localItems = JSON.parse( localStorage.getItem('localItem'))
+    taskList.splice(index, 1)
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    showItem()
+}
+
+
+    
